f41f8c4f4870c2d19156b386ca5c43434e25b778
jeimy <eloisarequeno@gmail.com>
2022-10-23 15:45:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pm2e17875/configuraciones/SQLiteConexion.java
@@ -0,0 +1,32 @@
+package com.example.pm2e17875.configuraciones;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+//import android.support.annotation.Nullable;
+
+import androidx.annotation.Nullable;
+
+
+public class SQLiteConexion extends SQLiteOpenHelper{
+
+    public SQLiteConexion(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL(Transacciones.createTableContact);
+        sqLiteDatabase.execSQL(Transacciones.CreateTablePaises);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+        sqLiteDatabase.execSQL(Transacciones.dropTableContact);
+        sqLiteDatabase.execSQL(Transacciones.DropTablePaises);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/example/pm2e17875/configuraciones/Transacciones.java
@@ -0,0 +1,31 @@
+package com.example.pm2e17875.configuraciones;
+
+public class Transacciones {
+
+    public static final String NameDatabase = "PM01DB";
+
+
+    public static String tblPaises = "paises";
+    public static final String codigo ="codigo";
+    public static final String p_pais="pais";
+
+    public static final String CreateTablePaises = "CREATE TABLE " + tblPaises + "(codigo INTEGER PRIMARY KEY,"+"pais TEXT )";
+    public static final String DropTablePaises = "DROP TABLE " + tblPaises;
+
+
+    public static final String tablacontactos = "contactos";
+
+        public static final String id = "id";
+        public static final String nombreCompleto = "nombreCompleto";
+        public static final String telefono = "telefono";
+        public static final String nota = "nota";
+        public static final String foto = "foto";
+        public static final String pais = "pais";
+
+
+        public static final String createTableContact = "CREATE TABLE " + tablacontactos +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "nombreCompleto TEXT, telefono INTEGER, nota TEXT, foto BLOB, pais TEXT)";
+
+        public static final String dropTableContact = "DROP TABLE IF EXIST" + tablacontactos;
+}
